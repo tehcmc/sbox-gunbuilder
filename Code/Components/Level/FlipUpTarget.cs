46572ed3280c7c96f3e03de2597fea29634956c8
Tony Ferguson <devultj@gmail.com>
2024-05-08 10:50:32 +0100

    Add flip up target sound, docs

--- Code/Components/Level/FlipUpTarget.cs
+++ Code/Components/Level/FlipUpTarget.cs
@@ -1,10 +1,43 @@
-using Sandbox;
-
+/// <summary>
+/// A flip up target. They'll ideally flip down when shot, then spring back up in a set time (by another system).
+/// </summary>
 public sealed class FlipUpTarget : Component
 {
+	/// <summary>
+	/// The target in question.
+	/// </summary>
 	[Property] public GameObject Target { get; set; }
-	[Property] public bool Triggered { get; set; } = false;
+
+	/// <summary>
+	/// A sound to play when triggering the target.
+	/// </summary>
+	[Property] public SoundEvent TriggerSound { get; set; }
+
+	/// <summary>
+	/// How quick?
+	/// </summary>
 	[Property] public float Speed { get; set; } = 10;
+
+	private bool triggered = false;
+	
+	/// <summary>
+	/// Is this target active? (Triggered = upright)
+	/// </summary>
+	[Property] public bool Triggered
+	{
+		get => triggered;
+		set
+		{
+			if ( triggered == value ) return;
+
+			triggered = value;
+
+			if ( TriggerSound is not null )
+			{
+				Sound.Play( TriggerSound, Transform.Position );
+			}
+		}
+	}
 
 	float SmoothPitch = 180;
 	protected override void OnUpdate()
--- Code/Components/Level/MovingTarget.cs
+++ Code/Components/Level/MovingTarget.cs
@@ -1,9 +1,24 @@
+
+/// <summary>
+/// A classic moving target. This is designed to be moved with input, by setting the distance using some buttons in physical space.
+/// </summary>
 public sealed class MovingTarget : Component
 {
+	/// <summary>
+	/// The object we're moving.
+	/// </summary>
 	[Property] public GameObject Object { get; set; }
+
+	/// <summary>
+	/// How quick are we?
+	/// </summary>
 	[Property] public float Speed { get; set; }
 
 	float dist = 0;
+
+	/// <summary>
+	/// How far (in meters) can this target go in any direction?
+	/// </summary>
 	[Property, Title( "Distance (meters)" )] public float Distance
 	{
 		get => dist;
@@ -14,6 +29,11 @@ public sealed class MovingTarget : Component
 		}
 	}
 
+	/// <summary>
+	/// Converts meters to inches (the engine's unit system).
+	/// </summary>
+	/// <param name="meters"></param>
+	/// <returns></returns>
 	float MetersToInches( float meters )
 	{
 		return (meters * 39.3701f);
